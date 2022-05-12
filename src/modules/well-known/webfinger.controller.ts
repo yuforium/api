f59480fa77325eb0a378ea83c400795bab5d1ad8
Chris Moser <cpmoser@gmail.com>
2022-05-11 22:18:02 -0700

    feat: mvp - fix webfinger bug

--- src/modules/well-known/webfinger.controller.ts
+++ src/modules/well-known/webfinger.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { BadRequestException, Controller, Get, NotFoundException, Query } from '@nestjs/common';
 import { ServiceId } from 'src/common/decorators/service-id.decorator';
 import { WebfingerService } from './webfinger.service';
 
@@ -7,8 +7,13 @@ export class WebfingerController {
   constructor(protected readonly webfingerService: WebfingerService) {}
 
   @Get()
-  public async webfinger(@Query('resource') resource: string): Promise<any> {
-    const [, username, serviceId] = /^acct:([A-Za-z0-9_]*)@(.*)$/i.exec(resource);
+  public async webfinger(@ServiceId() serviceId: string, @Query('resource') resource: string): Promise<any> {
+    const [, username, parsedServiceId] = /^acct:([A-Za-z0-9_]*)@(.*)$/i.exec(resource) || [];
+
+    if (!username || !parsedServiceId) {
+      throw new NotFoundException();
+    }
+
     const acct = this.webfingerService.getAccount(serviceId, username);
 
     return acct;
