abd7c8fd09f32478b08eca381a9344e69190ca5d
Chris Moser <cpmoser@gmail.com>
2022-12-26 13:10:59 -0800

    feat: mvp - wip

--- src/modules/activity-pub/activity-pub.module.ts
+++ src/modules/activity-pub/activity-pub.module.ts
@@ -5,10 +5,11 @@ import { ObjectModule } from '../object/object.module';
 import { ActivityPubService } from './services/activity-pub.service';
 import { InboxProcessorService } from './services/inbox-processor.service';
 import { InboxService } from './services/inbox.service';
+import { OutboxProcessorService } from './services/outbox-processor.service';
 import { OutboxService } from './services/outbox.service';
 
 @Module({
-  providers: [InboxService, OutboxService, ActivityPubService, InboxProcessorService],
+  providers: [InboxService, OutboxService, ActivityPubService, InboxProcessorService, OutboxProcessorService],
   exports: [InboxService, OutboxService],
   imports: [
     ActivityModule,
