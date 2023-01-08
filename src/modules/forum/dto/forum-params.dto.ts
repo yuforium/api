cf506e0e60b37873ae70c4d67d78525829ab19b5
Chris Moser <cpmoser@gmail.com>
2023-01-08 11:45:31 -0800

    feat: mvp - fix regexes

--- src/modules/forum/dto/forum-params.dto.ts
+++ src/modules/forum/dto/forum-params.dto.ts
@@ -3,7 +3,7 @@ import { Matches, IsString } from 'class-validator';
 
 export class ForumParams {
 	@ApiProperty({type: 'string', required: true})
-	@Matches(/^[a-z](?:-?[a-z0-9]+)*$/i)
+	@Matches(/^[a-z](?:-?[a-z0-9]+){3,255}$/i)
 	@IsString()
 	pathId: string | undefined;
 }
--- src/modules/user/dto/user-params.dto.ts
+++ src/modules/user/dto/user-params.dto.ts
@@ -3,7 +3,7 @@ import { IsNotEmpty, IsString, Matches, MaxLength, MinLength } from "class-valid
 export class UserParamsDto {
   @MinLength(5)
   @MaxLength(64)
-  @Matches(/^[a-z](?:-?[a-z0-9]+)*$/i)
+  @Matches(/^[a-z](?:-?[a-z0-9]+){3,255}$/i)
   @IsString()
   @IsNotEmpty()
   username!: string;
