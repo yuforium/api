4474491097d0f1a658076986ab7964eebd5e00c7
Chris Moser <cpmoser@gmail.com>
2023-07-02 18:47:16 +0200

    feat: mvp - fix TS error

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import database from './config/database';
 import service from "./config/service";
 import auth from './config/auth';
-import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseModule, MongooseModuleFactoryOptions } from '@nestjs/mongoose';
 import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
 import { WellKnownModule } from './modules/well-known/well-known.module';
@@ -18,7 +18,7 @@ import { ActivityModule } from './modules/activity/activity.module';
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: async (config: ConfigService) => config.get('database')
+      useFactory: async (config: ConfigService) => config.get('database') as MongooseModuleFactoryOptions
     }),
     AuthModule,
     UserModule,
